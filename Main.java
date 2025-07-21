3795b507660978c705470e14e7f0d1293e610c5a
juancamiloalbarracin <81522586+juancamiloalbarracin@users.noreply.github.com>
2025-07-21 10:01:00 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+import views.LoginView;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Initialize the application and set up the initial views
+        LoginView loginView = new LoginView();
+        loginView.setVisible(true);
+    }
+}
